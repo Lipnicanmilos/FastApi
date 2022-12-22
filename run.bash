0f61f4a8883ee50af1e8a69f7d4342c73f2233ac
Miloš Lipničan <lipnicanmilos@gmail.com>
2022-12-22 18:32:03 +0100

    asdf

---
+++ run.bash
@@ -0,0 +1,8 @@
+source fastapi-env/Scripts/activate
+uvicorn milos_fastapi:my_app --reload
+
+# -- http://127.0.0.1:8000/
+# uvicorn main:milos_app reload
+
+#    http://127.0.0.1:8000/docs/
+#    http://127.0.0.1:8000/redoc
